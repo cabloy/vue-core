bb731871975a3cc51a8ecfb716780386dd6836b3
zhennann <zhen.nann@icloud.com>
2024-07-03 16:03:19 +0800

    feat: controllerRef

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -151,7 +151,7 @@ class BaseReactiveHandler implements ProxyHandler<Target> {
 
     if (isRef(res)) {
       // ref unwrapping - skip unwrap for Array + integer key.
-      return targetIsArray && isIntegerKey(key) ? res : res.value
+      return targetIsArray && isIntegerKey(key) ? res : unrefNested(res)
     }
 
     if (isObject(res)) {
@@ -163,6 +163,10 @@ class BaseReactiveHandler implements ProxyHandler<Target> {
 
     return res
   }
+}
+
+function unrefNested(ref: any): any {
+  return isRef(ref) ? unrefNested(ref.value) : ref
 }
 
 class MutableReactiveHandler extends BaseReactiveHandler {
